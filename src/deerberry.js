8310ec5318faaf24118223bc51d24bc022694cd5
Li Xq <dyermaker@163.com>
2018-02-02 23:30:01 +0800

    deerberry.js

---
+++ src/deerberry.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @name pumelo
+ * 
+ * @author xqLi 
+ */
+
+(function (global, Fun) {
+    'use strict';
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = Fun : (global.deerberry = Fun);
+}(this, function(){
+	
+}))
